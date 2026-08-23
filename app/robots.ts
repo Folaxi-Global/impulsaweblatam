a3242aa54f30143cf7d3bb3874fea411a2810730
Folaxi-Global <viralesv519@gmail.com>
2026-08-23 00:00:23 -0400

    Change robots.txt to disallow checkout path

--- app/robots.ts
+++ app/robots.ts
@@ -1,11 +1,11 @@
 import { MetadataRoute } from 'next'
-
+ 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
-      disposable: ['/checkout'], // Evita que Google indexe los carritos de pago directos
+      disallow: ['/checkout'], // Evita que Google indexe los carritos de pago directos
     },
     sitemap: 'https://impulsaweblatam.com/sitemap.xml',
   }
