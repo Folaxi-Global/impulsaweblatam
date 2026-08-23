0783bb13aa1404fb16ff8b6d8e5fc6095ea4ed41
Folaxi-Global <viralesv519@gmail.com>
2026-08-22 23:38:16 -0400

    Add sitemap function for website metadata

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://impulsaweblatam.com',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://impulsaweblatam.com/crear',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+  ]
+}
