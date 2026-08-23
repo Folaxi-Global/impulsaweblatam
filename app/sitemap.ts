6763d0f5d29d3587af45a78e099ceb79215e1114
Folaxi-Global <viralesv519@gmail.com>
2026-08-23 00:01:43 -0400

    Fix sitemap function return type declaration

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
-
-export default function sitemap(): MetadataRoute.MetadataRoute.Sitemap {
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://impulsaweblatam.com',
